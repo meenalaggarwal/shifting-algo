5e301a940d0a222850188062bb6cc0b93827bda4
Meenal Aggarwal <meenala@one.com>
2019-02-23 13:33:17 +0530

    Shift Algorithm

--- shift.js
+++ shift.js
@@ -1,15 +1,32 @@
-var arr = [1, 4, 16, 8, 8, 2, 1];
+var arr = [1, 2, 4, 4, 8];
+var steps = [];
 
-for (var i = 0; i < arr.length; i++) {
+shift();
 
-	if (arr[i] === arr[i + 1]) {
-		var sum = arr[i] + arr[i + 1];
-		if ((i - 1 >= 0) && (sum === arr[i - 1])) {
-			//left
-		} else if ((i + 2 < arr.length) && (sum === arr[i + 2])) {
-			//right
-		} else {
-			//left
+function shift() {
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] === arr[i + 1]) {
+			var sum = arr[i] + arr[i + 1];
+			if ((i - 1 >= 0) && (sum === arr[i - 1])) {
+				//left shift
+				arr[i] = sum;
+				arr.splice(i + 1, 1);
+				steps.push('left');
+			} else if ((i + 2 < arr.length) && (sum === arr[i + 2])) {
+				//right shift
+				arr[i + 1] = sum;
+				arr.splice(i, 1);
+				steps.push('right');
+			} else {
+				//left shift
+				arr[i] = sum;
+				arr.splice(i + 1, 1);
+				steps.push('left');
+			}
+			return shift();
 		}
-	}
+	}	
 }
+
+console.log(arr)
+console.log(steps)
